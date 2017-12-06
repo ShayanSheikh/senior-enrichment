2cd54aa3c1dd50ed7eae9dcbb99d1ce7e7da5d7b
Shayan Sheikh <najam.shayan@gmail.com>
2017-12-06 15:53:31 -0500

    Working on views

---
+++ app/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Sup</h3>
+      </div>
+    )
+  }
+}
